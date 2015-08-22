20b3bd317049d6c9880b53e548d65ee1803b1907
Vidya Pati <vidyapati@microvac.co.id>
2015-08-22 15:31:41 +0700

    Haketon with entity framework

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -1,11 +1,21 @@
 ﻿namespace Haketon
 {
     using Nancy;
+    using Nancy.Conventions;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            conventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("scripts", @"Scripts")
+            );
+        }
     }
 }
---
+++ Models/IDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+
+namespace Haketon.Models
+{
+    public interface IDBContext
+    {
+        IDbSet<Test> Tests { get; set; }
+        DbEntityEntry Entry(object entity);
+        int SaveChanges();
+    }
+}
