51576cbdedd2f759838b21c4e993a18d215782ec
Gozali Kumara <ghk@gozalikumara.com>
2015-08-23 02:22:41 +0700

    dashboard

--- DashboardModule.cs
+++ DashboardModule.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Nancy;
+using Npgsql;
+using Dapper;
 
 namespace Haketon
 {
@@ -10,6 +12,16 @@ namespace Haketon
     {
         public DashboardModule()
         {
+            Get["/dashboard"] = parameters =>
+            {
+                using (var conn = new NpgsqlConnection(ApplicationConfig.CONNECTION_STRING))
+                {
+                    var datas = conn.Query<String>("select string_agg( commoditytype.name || '-' || order_type || ',' || price , '\\n') from orders inner join commoditytype on orders.commoditytype = commoditytype.id;").ToList();
+                    var data = datas[0];
+                    return View["dashboard", data];
+                };
+
+            };
             Get["/sunburst"] = parameters =>
             {
                 return View["sunburst", "bro"];
