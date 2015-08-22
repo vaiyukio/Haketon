46ea008c3c737e470b4fd2e765e00255b8a623f9
rifai <rifai@microvac.co.id>
2015-08-23 05:11:15 +0700

    change order_type to ordertype

--- DashboardModule.cs
+++ DashboardModule.cs
@@ -17,7 +17,7 @@ namespace Haketon
             {
                 using (var conn = new NpgsqlConnection(ApplicationConfig.CONNECTION_STRING))
                 {
-                    var datas = conn.Query<String>("select string_agg( commoditytype.name || '-' || order_type || ',' || price , '\\n') from orders inner join commoditytype on orders.commoditytype = commoditytype.id;").ToList();
+                    var datas = conn.Query<String>("select string_agg( commoditytype.name || '-' || ordertype || ',' || price , '\\n') from orders inner join commoditytype on orders.commoditytype = commoditytype.id;").ToList();
                     var data = datas[0];
                     return View["dashboard", data];
                 };
