9b22dd54fc1ab9b4bf5884982278a8a3f4942770
Riza Ramadan <riza.ramadan@sangkuriang.co.id>
2015-08-23 00:05:13 +0700

    dashboard

---
+++ DashboardModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+
+namespace Haketon
+{
+    class DashboardModule : NancyModule
+    {
+        public DashboardModule()
+        {
+            Get["/sunburst"] = parameters =>
+            {
+                return View["sunburst"];
+            };
+        }
+    }
+}
--- IndexModule.cs
+++ IndexModule.cs
@@ -27,7 +27,6 @@ namespace Haketon
                 };
                 return View["index"];
             };
-
         }
     }
 }
