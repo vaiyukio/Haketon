e174b01a2f96c16d0c12834c13cdc050c8d6c439
Gozali Kumara <ghk@gozalikumara.com>
2015-08-22 20:50:57 +0700

    add index module with dapper

---
+++ IndexModule.cs
@@ -0,0 +1,33 @@
+﻿using Nancy;
+using Npgsql;
+using Dapper;
+using System.Data;
+using System.Linq;
+using Haketon.Models;
+
+namespace Haketon
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            string connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=;Database=haketon;Encoding=UNICODE";
+
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+
+            Get["/test"] = parameters =>
+            {
+                using (var conn = new NpgsqlConnection(connectionString))
+                {
+                    var tests = conn.Query<User>("SELECT Id, Name FROM test");
+                    var anu = tests.Count();
+                };
+                return View["index"];
+            };
+
+        }
+    }
+}
