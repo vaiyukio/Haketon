bd3b6281c3421b8cda4d7feb2f5aec2f6e0c65bd
Gozali Kumara <ghk@gozalikumara.com>
2015-08-22 20:25:35 +0700

    add models

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Haketon.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public String KtpNumber { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Address { get; set; }
+        public double Long { get; set; }
+        public double Lat { get; set; }
+    }
+}
