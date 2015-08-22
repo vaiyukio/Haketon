b9cc1a1ef3a98ee50a66df579a7f0108f5142d20
rifai <rifai@microvac.co.id>
2015-08-23 00:05:07 +0700

    Add sunburst

---
+++ DasboardModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy;
+using Haketon.Models;
+using Haketon.USSD;
+
+namespace Haketon
+{
+    public class DasboardModule : NancyModule
+    {
+        public DasboardModule()
+        {
+            Get["/sunburst"] = parameters => 
+            {
+                return View["sunburst"];
+            };
+        }
+    }
+}
---
+++ USSD/RegistrationResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Npgsql;
+using Dapper;
+using Haketon.Models;
+
+namespace Haketon.USSD
+{
+    public class RegistrationResolver
+    {
+    }
+}
